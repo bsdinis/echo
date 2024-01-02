15628e0be5df6be4ab9700784db4bc9f089e0749
bsdinis <bsdinis@mit.edu>
2024-01-02 23:29:35 +0000

    [echo/python]: python echo server

---
+++ python/client.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import click
+import humanfriendly
+import logging
+import socket
+import time
+import multiprocessing
+
+BUCKET_SIZE = 2 ** 15
+BUCKET = b'\x42' * BUCKET_SIZE
+
+
+def run_client(host, port, message_size):
+    message_size = humanfriendly.parse_size(message_size, binary=True)
+    encoded_size = message_size.to_bytes(8, 'big')
+
+    size_to_send = message_size
+
+    addr = (host, port)
+    server = socket.create_connection(addr)
+
+    start_time = time.perf_counter()
+    assert server.send(encoded_size) == 8
+
+    while size_to_send > 0:
+        this_bucket = min(BUCKET_SIZE, size_to_send)
+        logging.debug(
+            'sending message ({}/{})'.format(this_bucket, size_to_send))
+        sent = server.send(BUCKET[:this_bucket])
+        size_to_send -= sent
+
+    received = 0
+    while received < message_size:
+        logging.debug('receiving message ({})'.format(message_size - received))
+        bucket = server.recv(message_size - received)
+
+        if bucket is None or len(bucket) == 0:
+            logging.debug('empty echo')
+            return -1
+
+        logging.debug('message size: {}'.format(len(bucket)))
+        received += len(bucket)
+        if len(bucket.strip(b'\x42')) > 0:
+            logging.debug("unsuccessful echo")
+            return -1
+
+    end_time = time.perf_counter()
+    server.close()
+    return end_time - start_time
+
+
+def run_worker(host, port, message_size, queue, reps):
+    for _ in range(reps):
+        try:
+            elapsed = run_client(host, port, message_size)
+            queue.put(elapsed)
+        except BaseException:
+            logging.exception(
+                'failed to run client workload @ {}:{}'.format(host, port))
+
+
+@click.command("client")
+@click.argument("host", default="localhost", type=click.STRING)
+@click.argument("port", default=9090, type=click.INT)
+@click.option("--n-cores", "-j", type=click.INT,
+              default=multiprocessing.cpu_count(), show_default=True)
+@click.option("--reps", "-r", type=click.INT, default=1000, show_default=True)
+@click.option("--message-size",
+              "-s",
+              default="1",
+              show_default=True,
+              type=click.STRING,
+              help="message size in B")
+@click.option("--verbose", '-v', is_flag=True, type=click.BOOL, default=False)
+def client(host, port, n_cores, reps, message_size, verbose):
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
+    queue = multiprocessing.Queue(reps)
+    workers = [
+        multiprocessing.Process(
+            target=run_worker, args=(
+                host, port, message_size, queue, reps // n_cores)) for _ in range(
+            n_cores - 1)] + [
+        multiprocessing.Process(
+            target=run_worker, args=(
+                host, port, message_size, queue, reps // n_cores + reps %
+                n_cores))]
+    for w in workers:
+        w.start()
+    for w in workers:
+        w.join()
+
+    elapsed = list()
+    while queue.empty() == False:
+        elapsed.append(queue.get())
+    print('\n'.join('{:.3f}us'.format(10**6 * e) for e in elapsed))
+
+
+if __name__ == "__main__":
+    client()
---
+++ python/server.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import click
+import humanfriendly
+import logging
+import socket
+import multiprocessing
+
+BUCKET_SIZE = 2 ** 16
+
+
+def echo(worker_id, client, address, data):
+    sent = 0
+    while sent < len(data):
+        logging.debug('[{}] sending message ({}/{}) [@{}:{}]'.format(worker_id,
+                      len(data) - sent, len(data), *address))
+        sent = client.send(data[sent:])
+
+
+def handle_client(worker_id, client, address):
+    logging.info('[{}] handling client @ {}:{}'.format(worker_id, *address))
+    encoded_size = client.recv(8)
+    message_size = int.from_bytes(encoded_size, 'big')
+
+    size_to_receive = message_size
+    while size_to_receive > 0:
+        logging.debug('[{}] receiving message ({}/{}) [@{}:{}]'.format(worker_id,
+                      message_size - size_to_receive, message_size, *address))
+        this_bucket = min(BUCKET_SIZE, size_to_receive)
+        data = client.recv(this_bucket)
+        if data:
+            size_to_receive -= len(data)
+            echo(worker_id, client, address, data)
+        else:
+            logging.warning(
+                '[{}] failed to receive a message from client @ {}:{}'.format(worker_id, *address))
+            break
+
+    client.close()
+
+
+def worker(worker_id, queue):
+    while True:
+        client, address = queue.get()
+        try:
+            handle_client(worker_id, client, address)
+        except BaseException:
+            logging.exception(
+                '[{}] failed to handle client @ {}:{}'.format(worker_id, *address))
+
+
+@click.command("server")
+@click.argument("host", type=click.STRING, default="localhost")
+@click.argument("port", type=click.INT, default=9090)
+@click.option("--n-cores", "-j", type=click.INT,
+              default=multiprocessing.cpu_count(), show_default=True)
+@click.option("--backlog", '-b', type=click.INT, default=None)
+@click.option("--verbose", '-v', is_flag=True, type=click.BOOL, default=False)
+def server(host, port, n_cores, backlog, verbose):
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
+    addr = (host, port)
+    s = socket.create_server(addr, backlog=backlog)
+    queue = multiprocessing.Queue()
+    workers = [
+        multiprocessing.Process(
+            target=worker,
+            args=(
+                i + 1,
+                queue)) for i in range(
+            n_cores - 1)]
+    for w in workers:
+        w.start()
+
+    while True:
+        client, address = s.accept()
+        queue.put((client, address))
+
+    for w in workers:
+        w.join()
+
+
+if __name__ == "__main__":
+    server()
